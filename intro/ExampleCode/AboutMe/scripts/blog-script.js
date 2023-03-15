5c33f2d968b3eeccbc3e0f0e7f8a4ca8df91a004
Quasar <trottinger25@pascack.org>
2023-03-15 11:46:28 -0400

    got the shit working

--- intro/ExampleCode/AboutMe/scripts/blog-script.js
+++ intro/ExampleCode/AboutMe/scripts/blog-script.js
@@ -1,30 +1,46 @@
-let blogs = [
-    {"title": "blog 1", "desc":"description 1"},
-    {"title": "blog 2", "desc":"description 2"},
-    {"title": "blog 3", "desc":"description 3"},
-    {"title": "blog 4", "desc":"description 4"},
-    {"title": "blog 5", "desc":"description 5"},
-];
+// let blogs = [
+//     {"title": "blog 1", "desc":"description 1"},
+//     {"title": "blog 2", "desc":"description 2"},
+//     {"title": "blog 3", "desc":"description 3"},
+//     {"title": "blog 4", "desc":"description 4"},
+//     {"title": "blog 5", "desc":"description 5"},
+// ];
+
+// fetch('https://club.rottinger.net/api/blogs', { mode: "no-cors"})
+// .then((response) => {
+//     response.json()
+//     .then((data) => {
+//         console.log(data); 
+//         addToPage(data);
+//     })
+// });
+
+fetch("https://club.rottinger.net/api/blogs")
+  .then((response) => response.json())
+  .then((data) => addToPage(data));
 
 
-for (let i = 0; i < blogs.length; i++)
+function addToPage(data)
 {
-    let child = document.createElement("div");
-
-    let title = document.createElement("h2");
-    title.textContent = blogs[i].title;
-
-    let description = document.createElement("p");
-    description.textContent = blogs[i].desc;
-
-    if(i == 0)
-        child.classList.add("table-item-top")
-    if(i == blogs.length - 1)
-        child.classList.add("table-item-bottom")
-
-
-    child.classList.add("table-item")
-    child.appendChild(title)
-    child.appendChild(description)
-    document.getElementById("blog-table").appendChild(child);
+    for (let i = 0; i < data.length; i++)
+    {
+        let child = document.createElement("div");
+    
+        let title = document.createElement("h2");
+        title.textContent = data[i].title;
+    
+        let description = document.createElement("p");
+        description.textContent = data[i].description;
+    
+        if(i == 0)
+            child.classList.add("table-item-top")
+        if(i == data.length - 1)
+            child.classList.add("table-item-bottom")
+    
+    
+        child.classList.add("table-item")
+        child.appendChild(title)
+        child.appendChild(description)
+        document.getElementById("blog-table").appendChild(child);
+    }
 }
