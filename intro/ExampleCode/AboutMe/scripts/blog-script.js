0f02184dd2078948d6ae2d2810a706e2c3867d67
Quasar <trottinger25@pascack.org>
2023-03-16 14:58:34 -0400

    connection to database

--- intro/ExampleCode/AboutMe/scripts/blog-script.js
+++ intro/ExampleCode/AboutMe/scripts/blog-script.js
@@ -1,23 +1,28 @@
-// let blogs = [
-//     {"title": "blog 1", "desc":"description 1"},
-//     {"title": "blog 2", "desc":"description 2"},
-//     {"title": "blog 3", "desc":"description 3"},
-//     {"title": "blog 4", "desc":"description 4"},
-//     {"title": "blog 5", "desc":"description 5"},
-// ];
-
-// fetch('https://club.rottinger.net/api/blogs', { mode: "no-cors"})
-// .then((response) => {
-//     response.json()
-//     .then((data) => {
-//         console.log(data); 
-//         addToPage(data);
-//     })
-// });
-
 fetch("https://club.rottinger.net/api/blogs")
-  .then((response) => response.json())
-  .then((data) => addToPage(data));
+.then((response) => response.json())
+.then((data) => {
+    for (let i = 0; i < data.length; i++)
+    {
+        let child = document.createElement("div");
+
+        let title = document.createElement("h2");
+        title.textContent = data[i].title;
+
+        let description = document.createElement("p");
+        description.textContent = data[i].description;
+
+        if(i == 0)
+            child.classList.add("table-item-top")
+        if(i == data.length - 1)
+            child.classList.add("table-item-bottom")
+
+
+        child.classList.add("table-item")
+        child.appendChild(title)
+        child.appendChild(description)
+        document.getElementById("blog-table").appendChild(child);
+    }
+});
 
 
 function addToPage(data)
