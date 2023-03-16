8ac3a5c76cd60deffe871a82200c8f9ee6c844fb
MrV1ctor <vurumov25@pascack.org>
2023-03-16 14:58:57 -0400

    blog meeting

--- intro/AboutMe/scripts/blogs-script.js
+++ intro/AboutMe/scripts/blogs-script.js
@@ -1,7 +1,44 @@
-let paragraph = "text text text text text"; 
-let description = document.createElement("p");
+let blogs = [
+    {
+        "title" : "blog 1",
+        "desc" : "description 1"
+    },
+    {
+        "title" : "blog 2",
+        "desc" : "description 2"
+    },
+    {
+        "title" : "blog 3",
+        "desc" : "description 3"
+    },
+    {
+        "title" : "blog 4",
+        "desc" : "description 4"
+    },
+    {
+        "title" : "blog 5",
+        "desc" : "description 5"
+    }
+];
 
-description.textContent = paragraph;
-description.classList.add("desc");
+for (let i = 0; i < blogs.length; i++)
+{
+    let blogItem = document.createElement("div");
 
-document.querySelector("body").appendChild(description);
+    let paragraph = blogs[i].title; 
+    let title = document.createElement("h4");
+    title.textContent = paragraph;
+
+    blogItem.classList.add("blog-item");
+
+    blogItem.appendChild(title);
+    
+
+    let description = document.createElement("p");
+    description.textContent = blogs[i].desc;
+
+    blogItem.appendChild(description);
+    
+    document.querySelector("body").appendChild(blogItem);
+}
+
