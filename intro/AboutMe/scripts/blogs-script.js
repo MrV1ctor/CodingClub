8f6f0f941953e25b67aa68ac42a87baeca84efd4
MrV1ctor <vurumov25@pascack.org>
2023-03-17 12:56:46 -0400

    meeeting

--- intro/AboutMe/scripts/blogs-script.js
+++ intro/AboutMe/scripts/blogs-script.js
@@ -1,44 +1,27 @@
-let blogs = [
+fetch("https://club.rottinger.net/api/blogs")
+.then((response) => response.json())
+.then((blogs) => {
+    
+    for (let i = 0; i < blogs.length; i++)
     {
-        "title" : "blog 1",
-        "desc" : "description 1"
-    },
-    {
-        "title" : "blog 2",
-        "desc" : "description 2"
-    },
-    {
-        "title" : "blog 3",
-        "desc" : "description 3"
-    },
-    {
-        "title" : "blog 4",
-        "desc" : "description 4"
-    },
-    {
-        "title" : "blog 5",
-        "desc" : "description 5"
+        let blogItem = document.createElement("div");
+    
+        let paragraph = blogs[i].title; 
+        let title = document.createElement("h4");
+        title.textContent = paragraph;
+    
+        blogItem.classList.add("blog-item");
+    
+        blogItem.appendChild(title);
+        
+    
+        let description = document.createElement("p");
+        description.textContent = blogs[i].description;
+    
+        blogItem.appendChild(description);
+        
+        document.getElementById("blog-table").appendChild(blogItem);
     }
-];
 
-for (let i = 0; i < blogs.length; i++)
-{
-    let blogItem = document.createElement("div");
-
-    let paragraph = blogs[i].title; 
-    let title = document.createElement("h4");
-    title.textContent = paragraph;
-
-    blogItem.classList.add("blog-item");
-
-    blogItem.appendChild(title);
-    
-
-    let description = document.createElement("p");
-    description.textContent = blogs[i].desc;
-
-    blogItem.appendChild(description);
-    
-    document.querySelector("body").appendChild(blogItem);
-}
+})
 
