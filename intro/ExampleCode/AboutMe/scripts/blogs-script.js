92191884430c5fb33794dfeca833fc6796a6fa92
MrV1ctor <vurumov25@pascack.org>
2023-03-23 14:56:23 -0400

    fixed id shizz

--- intro/ExampleCode/AboutMe/scripts/blogs-script.js
+++ intro/ExampleCode/AboutMe/scripts/blogs-script.js
@@ -21,7 +21,7 @@ fetch("https://club.rottinger.net/api/blogs")
     
             let link = document.createElement("a");
     
-            link.href = (window.location.href.substring(0, window.location.href.indexOf("?"))) + "?b="+(blogs[i].id-1);
+            link.href = (window.location.href.substring(0, window.location.href.indexOf("?"))) + "?b="+i;
     
             link.appendChild(title);
             
@@ -81,19 +81,3 @@ fetch("https://club.rottinger.net/api/blogs")
     }
     
 })
-
-//VICTOR'S NOTE
-//currently doesnt work for last one because of missing number 8, id 11-1 is out of bounds for length 9
-
-//printed in console...
-// (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-// 0: {id: 1, title: 'Learning HTML!', description: 'Follow my journey as I learn HTML!!', createdAt: '2023-03-10T01:24:44.000Z', updatedAt: '2023-03-10T01:24:44.000Z'}
-// 1: {id: 2, title: 'Learning CSS!', description: 'Follow my journey as I learn CSS!!', createdAt: '2023-03-10T01:25:04.000Z', updatedAt: '2023-03-10T01:25:04.000Z'}
-// 2: {id: 3, title: 'Learning JS!', description: 'Follow my journey as I learn JS!!', createdAt: '2023-03-10T01:25:10.000Z', updatedAt: '2023-03-10T01:25:10.000Z'}
-// 3: {id: 4, title: 'Devlog 1', description: 'Learning how to use elements in HTML', createdAt: '2023-03-10T01:26:20.000Z', updatedAt: '2023-03-10T01:26:20.000Z'}
-// 4: {id: 5, title: 'Devlog 2', description: 'Learning how to properly format my HTML document', createdAt: '2023-03-10T01:26:37.000Z', updatedAt: '2023-03-10T01:26:37.000Z'}
-// 5: {id: 6, title: 'Devlog 3', description: 'Learning how to properly format my HTML document', createdAt: '2023-03-10T01:26:50.000Z', updatedAt: '2023-03-10T03:10:05.000Z'}
-// 6: {id: 7, title: 'Devlog 4', description: 'Learning how to write JS', createdAt: '2023-03-10T01:27:28.000Z', updatedAt: '2023-03-10T01:27:28.000Z'}
-// 7: {id: 9, title: 'Devlog 4', description: 'Uh oh! We have a duplicate title!!', createdAt: '2023-03-16T12:39:43.000Z', updatedAt: '2023-03-16T12:39:43.000Z'}
-// 8: {id: 10, title: 'Devlog 5', description: 'Learning how to link a data base to our pages!', createdAt: '2023-03-16T12:40:48.000Z', updatedAt: '2023-03-16T12:40:48.000Z'}
-// 9: {id: 11, title: 'Test', description: 'Test1', createdAt: '2023-03-16T12:41:26.000Z', updatedAt: '2023-03-16T12:41:26.000Z'}
